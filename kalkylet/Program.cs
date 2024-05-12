70730c8bfe3c0f3bb9b57fe284d7b34ec579d6c6
Petrov Vladimir <123321djdf@mail.ru>
2024-05-12 22:00:28 +0300

    dobavil

---
+++ kalkylet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Простой калькулятор на C#");
+
+            Console.Write("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите оператор (+, -, *, /): ");
+            char operation = Console.ReadKey().KeyChar;
+            Console.WriteLine();  // Перенос строки после оператора
+
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: деление на ноль.");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неверный оператор.");
+                    return;
+            }
+
+            Console.WriteLine($"Результат: {num1} {operation} {num2} = {result}");
+        }
+    }
+}
